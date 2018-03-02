966c8315ab1337c28cfe93f66aa21f1380a5bc35
RyanDeSummers <rjshp42@gmail.com>
2018-03-02 13:51:24 -0800

    ye

--- app/src/main/java/com/example/summers/learningbackendless/RestaurantActivity.java
+++ app/src/main/java/com/example/summers/learningbackendless/RestaurantActivity.java
@@ -1,9 +1,13 @@
 package com.example.summers.learningbackendless;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class RestaurantActivity extends AppCompatActivity {
+    private TextView textGenre, textName, textCost, textAddress;
+    private RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
